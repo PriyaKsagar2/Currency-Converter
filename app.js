82a048cb18a8eb651771a15d516b0b09ceeec863
Priya Kishor Kshirsagar <2019bec134@sggs.ac.in>
2024-03-24 20:15:49 +0530

    Updated current currency api in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -6,37 +6,37 @@ const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
 
-for (let select of dropdowns){
-    for (currCode in countryList){
-        let newOption  = document.createElement("option");
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
-        if(select.name === "from" && currCode === "USD"){
+        if (select.name === "from" && currCode === "USD") {
             newOption.selected = "selected";
-        }else if(select.name === "to" && currCode === "INR"){
+        } else if (select.name === "to" && currCode === "INR") {
             newOption.selected = "selected";
         }
         select.append(newOption);
     }
 
-    select.addEventListener("change", (evt) =>{
-         updateFlag(evt.target);
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
     })
 }
 
-const updateExchangeRate = async ()=>{
+const updateExchangeRate = async () => {
     let amount = document.querySelector(".amount input");
     let amtVal = amount.value;
-    if(amtVal === "" || amtVal < 1){
+    if (amtVal === "" || amtVal < 1) {
         amtVal = 1;
         amount.value = "1";
     }
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
     let response = await fetch(URL);
     let data = await response.json();
-    let rate = data[toCurr.value.toLowerCase()];
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
 
-    let finalAmount = amtVal*rate;
+    let finalAmount = amtVal * rate;
     msg.innerText = `${amtVal}${fromCurr.value} = ${finalAmount}${toCurr.value}`;
 }
 
@@ -49,7 +49,7 @@ const updateFlag = (element) => {
 };
 
 btn.addEventListener("click", (evt) => {
-    evt.preventDefault(); // Corrected typo
+    evt.preventDefault();
     updateExchangeRate();
 });
 
